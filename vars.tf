1dbe7809972dc8ac29d789593607937f1641965e
Guilherme Pontes <ps.guilhermepontes@vix.com.br>
2023-01-27 16:07:01 -0300

    Alterando variáveis

--- vars.tf
+++ vars.tf
@@ -1,7 +1,3 @@
-variable "client_secret" {
-  default = "Oqq8Q~x8xFggiOi-jsC9FokiDW8XdrtkZUPctaYD"
-}
-
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
   default     = "West Europe"
